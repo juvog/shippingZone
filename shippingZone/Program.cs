cf3219b6ee372c071b4d8a216a923ae33664418c
juvog <blablabla@blablabla.com>
2024-02-27 09:07:25 -0500

    add user input and calculate fees

---
+++ shippingZone/Program.cs
@@ -0,0 +1,58 @@
+﻿using shippingZone;
+using System;
+
+FeesDelegate theDel;
+Double thePrice;
+string theZone;
+Destination theDest;
+
+do
+{
+    Console.WriteLine("What is the destination zone?");
+    theZone = Console.ReadLine();
+
+    if (theZone != "exit" & theZone != null)
+    {
+        // Find destination of shipping
+        theDest = Destination.GetDestination(theZone);
+    
+        if (theDest != null)
+        {
+            Console.WriteLine("What is the price of shipping?");
+            string thePriceStr = Console.ReadLine();
+            if (thePriceStr != null)
+            {
+                // price of shipping
+                thePrice = Double.Parse(thePriceStr);
+
+
+                // the delegates that calculates fees change when destination changes
+                theDel = theDest.calculateFees;
+                if (theDest.isHighRisk)
+                {
+                    theDel += delegate (double thePrice, ref double theFee)
+                    {
+                        theFee += 25;
+                    };
+                }
+
+                // we can now use the delegate to calculate fees
+                double theFee = 0;
+                theDel(thePrice, ref theFee);
+                Console.WriteLine($"The destination fees are : {theFee} $");
+            }
+        
+        }
+        else
+        {
+            Console.WriteLine("Wrong destination. Try again or type 'exit'");
+        }
+        
+
+    }
+        
+}
+while (theZone != "exit");
+
+
+
---
+++ shippingZone/ShippingDestination.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shippingZone
+{
+    public delegate void FeesDelegate(double thePrice, ref double fee);
+    abstract class Destination
+    {
+        public bool isHighRisk;
+
+        public virtual void calculateFees(double price, ref double fee) { }
+
+        public static Destination GetDestination(string destination)
+        {
+            if (destination == "zone1")
+            {
+                return new Destination1();
+            }
+            if (destination == "zone2")
+            {
+                return new Destination2();
+            }
+            if (destination == "zone3")
+            {
+                return new Destination3();
+            }
+            if (destination == "zone4")
+            {
+                return new Destination4();
+            }
+            return null;
+        }
+
+    }
+
+    internal class Destination4 : Destination
+    {
+        public Destination4()
+        {
+            this.isHighRisk = false;
+        }
+
+        public override void calculateFees(double price, ref double fee)
+        {
+            fee = price * 0.04;
+        }
+    }
+
+    internal class Destination3 : Destination
+    {
+        public Destination3()
+        {
+            this.isHighRisk = false;
+        }
+
+        public override void calculateFees(double price, ref double fee)
+        {
+            fee = price * 0.08;
+        }
+    }
+
+    internal class Destination2 : Destination
+    {
+        public Destination2()
+        {
+            this.isHighRisk = true;
+        }
+
+        public override void calculateFees(double price, ref double fee)
+        {
+            fee = price * 0.12;
+        }
+    }
+
+    internal class Destination1 : Destination
+    {
+        public Destination1()
+        {
+            this.isHighRisk = false;
+        }
+
+        public override void calculateFees(double price, ref double fee)
+        {
+            fee = price * 0.25;
+        }
+
+    }
+}
